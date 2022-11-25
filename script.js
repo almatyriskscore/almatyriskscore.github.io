8af449716856e7217973629b84983c1bf984b6d4
almatyriskscore <118692807+almatyriskscore@users.noreply.github.com>
2022-11-25 22:53:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ function functionName() {
     var second = document.getElementById('id_second').value;
     var third = document.getElementById('id_third').value;
     var fourth = document.getElementById('id_fourth').value;
-    var fifth = document.getElementById('id_fifth').value;
-    var sixth = document.getElementById('id_sixth').value;
-    var seventh = document.getElementById('id_seventh').value;
-    var eighth = document.getElementById('id_eighth').value;
 
     let answer_first = 0.0;
     let answer_second = 0.0;
@@ -33,86 +29,45 @@ function functionName() {
     if (parseFloat(fourth) < 40)
         answer_first += 5.3;
 
-    if (fifth.toLowerCase() == "да")
+    if (document.getElementById('gridRadios3').checked == true)
         answer_first += 5.2;
-    else if (fifth.toLowerCase() == "нет")
-        answer_first += 0;
-    else
-        document.getElementById("answer_first").textContent = "Ошибка";
 
-    if (sixth.toLowerCase() == "да")
+    if (document.getElementById('gridRadios5').checked == true)
         answer_first += 7.3;
-    else if (sixth.toLowerCase() == "нет")
-        answer_first += 0;
-    else
-        document.getElementById("answer_first").textContent = "Ошибка";
 
-    if (seventh.toLowerCase() == "да")
+    if (document.getElementById("selectFirst").value == "4")
         answer_first += 43;
-    else if (seventh.toLowerCase() == "нет")
-        answer_first += 0;
-    else
-        document.getElementById("answer_first").textContent = "Ошибка";
-
-    if (eighth.toLowerCase() == "да")
+    
+    if (document.getElementById('gridRadios9').checked == true)
         answer_first += 20.9;
-    else if (eighth.toLowerCase() == "нет")
-        answer_first += 0;
-    else
-        document.getElementById("answer_first").textContent = "Ошибка";
 
     if (document.getElementById("answer_first").textContent == "")
         document.getElementById("answer_first").textContent = "Вероятность исхода в: " + answer_first + " %";
 
-
-
     
-    if (parseFloat(first) < 120) {
+    if (parseFloat(first) < 120)
         answer_second += 2;
-    }
 
-    if (parseFloat(second) > 120) {
+    if (parseFloat(second) > 120)
         answer_second += 3;
-    }
 
-    if (parseFloat(third) > 5) {
+    if (parseFloat(third) > 5)
         answer_second += 2;
-    }
 
-    if (parseFloat(fourth) < 40) {
+    if (parseFloat(fourth) < 40)
         answer_second += 2;
-    }
 
-    if (fifth.toLowerCase() == "да")
+    if (document.getElementById('gridRadios3').checked == true)
         answer_second += 2;
-    else if (fifth.toLowerCase() == "нет")
-        answer_second += 0;
-    else
-        document.getElementById("answer_second").textContent = "Ошибка";
 
-
-    if (sixth.toLowerCase() == "да")
+    if (document.getElementById('gridRadios5').checked == true)
         answer_second += 3;
-    else if (sixth.toLowerCase() == "нет")
-        answer_second += 0;
-    else
-        document.getElementById("answer_second").textContent = "Ошибка";
 
-
-    if (seventh.toLowerCase() == "да")
+    if (document.getElementById("selectFirst").value == "4")
         answer_second += 8;
-    else if (seventh.toLowerCase() == "нет")
-        answer_second += 0;
-    else
-        document.getElementById("answer_second").textContent = "Ошибка";
-    
 
-    if (eighth.toLowerCase() == "да")
+    if (document.getElementById('gridRadios9').checked == true)
         answer_second += 6;
-    else if (eighth.toLowerCase() == "нет")
-        answer_second += 0;
-    else
-        document.getElementById("answer_second").textContent = "Ошибка";
     
     if (document.getElementById("answer_second").textContent == "")
         document.getElementById("answer_second").textContent = "Итоговые баллы: " + answer_second;
